f5240c77cf113725af5ddd3217b4853f1658af52
Luna <44528100+pozm@users.noreply.github.com>
2022-04-30 21:27:43 +0100

    update

--- NoHudMod/Patches/MapPlayerHooks.cs
+++ NoHudMod/Patches/MapPlayerHooks.cs
@@ -1,6 +1,36 @@
-﻿namespace NoHudMod.Patches;
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace NoHudMod.Patches;
 
 public class MapPlayerHooks
 {
+
+    static void HideAllGameObjects(GameObject obj)
+    {
+        Plugin.Log.LogInfo($"new : {obj.name} & {obj.transform.childCount}");
+        obj.SetActive(false);
+        // for(int i=0; i<obj.transform.childCount; i++)
+        // {
+        //     var newObj = obj.transform.GetChild(i).gameObject;
+        //     newObj.SetActive(false);
+        //     Plugin.Log.LogInfo(newObj.name);
+        //     HideAllGameObjects(newObj);
+        // }
+    }
     
+    [HarmonyPatch(typeof(MapPlayer), "FixedUpdate")]
+    [HarmonyPrefix]
+    static void OnFixedUpdate(MapPlayer __instance)
+    {
+        Plugin.Log.LogInfo($"p1 : {__instance.p1} ");
+        if (__instance.p1)
+        {
+            HideAllGameObjects(__instance.p1);
+            HideAllGameObjects(__instance.p2);
+            // __instance.p1.SetActive(false);
+            // __instance.p2.SetActive(false);
+            
+        }
+    }
 }
--- NoHudMod/Plugin.cs
+++ NoHudMod/Plugin.cs
@@ -1,15 +1,22 @@
 ﻿using BepInEx;
 using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+using NoHudMod.Patches;
 
 namespace NoHudMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BasePlugin
     {
+        internal static new ManualLogSource Log;
         public override void Load()
         {
             // Plugin startup logic
-            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            base.Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Plugin.Log = base.Log;
+            Harmony.CreateAndPatchAll(typeof(MapPlayerHooks));
+            
         }
     }
 }
